46105273df79db196e5656cf715914100aee92ce
DukeSmile <mitchelldev232@gmail.com>
2022-12-07 00:53:52 +0900

    feat: Add contract project

---
+++ certification-contract/contracts/BUCertification.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.14;
+
+contract BUCertificatioin is AccessControl {
+    
+    enum CharityType {
+        CHARITY,
+        FUNDRAISER
+    }
+}
---
+++ certification-contract/test/test.ts
@@ -0,0 +1,52 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { Contract } from "ethers";
+import { ethers } from "hardhat";
+import Web3 from "web3";
+import env from "dotenv";
+const { expect } = require("chai");
+
+describe("DDAContract Test network", () => {
+  let Token;
+  let universityCert: Contract,
+    deployer: SignerWithAddress,
+    donater1: SignerWithAddress,
+    donater2: SignerWithAddress,
+    fundRaiser1: SignerWithAddress,
+    fundRaiser2: SignerWithAddress,
+    charity1: SignerWithAddress;
+
+  describe("Deploying BUCertificatioin", () => {
+    it("Set accounts", async () => {
+      [deployer, donater1, donater2, fundRaiser1, fundRaiser2, charity1] = await ethers.getSigners();
+      console.log("deployer: ", deployer.address);
+    });
+    it("Deployed university certification contract", async () => {
+      console.log("Deploying BUCertificatioin");
+      const UBCcontract = await ethers.getContractFactory("BUCertificatioin");
+      universityCert = await UBCcontract.deploy(deployer.address);
+      await universityCert.deployed();
+      console.log("BUCcontract address: ", universityCert.address);
+      console.log(
+        "universityCert verify: ",
+        `npx hardhat verify --contract "contracts/BUCertificatioin.sol:BUCertificatioin" --network bscTestnet ${universityCert.address} ${deployer.address}`
+        );
+    });
+
+  });
+});
+
+function getFinalDonation(donation){
+  let val = parseInt(donation);
+  if (val >= 250000) {
+    val *= 0.999;
+  } else if (val >= 100000) {
+    val *= 0.997
+  } else if (val >= 50000) {
+    val *= 0.995
+  } else if ( val >= 10000) {
+    val *= 0.993
+  } else {
+    val *= 0.99;
+  }
+  return val;
+}
