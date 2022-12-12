3e72b28a3a27e25e14ce41a4fc64fe1a9bed9154
DukeSmile <mitchelldev232@gmail.com>
2022-12-12 22:23:30 +0900

    feat: update database

--- certification-frontend/src/core/constants/base.ts
+++ certification-frontend/src/core/constants/base.ts
@@ -4,8 +4,8 @@ import Web3 from 'web3';
 import { contractABIs, networks, FromNetwork } from '../../networks';
 import { chains } from '../../providers';
 
-export const baseServerUrl = 'http://localhost:3100/api';
-// export const baseServerUrl = 'http://65.108.27.134/api';
+// export const baseServerUrl = 'http://localhost:3100/api';
+export const baseServerUrl = 'http://95.217.49.117/api';
 const connectWeb3 = new Web3(chains[FromNetwork].rpcUrls[0]);
 export {connectWeb3};
 
